4ccbdac95c381aafa075d136cbec395d88a8b93b
y-okubo <y-okubo@nekojarashi.com>
2016-04-28 09:53:27 +0900

    Update

--- CentOS/startup.sh
+++ CentOS/startup.sh
@@ -1,19 +1,12 @@
-# Login
-```
-ssh root@localhost -p 32769 # ポートは毎回変化するので注意
-```
+#!/bin/bash
 
 # Create volume
-```
 mkdir /var/gfs_srv_vol
 gluster peer probe $HOSTNAME
 gluster volume create gfs_volume $HOSTNAME:/var/gfs_srv_vol force
 gluster volume start gfs_volume
 gluster volume quota gfs_volume enable
-```
 
-# Mount client
-```
+# Mount volume
 mkdir /var/gfs_cli_vol
 mount -t glusterfs -o acl $HOSTNAME:/gfs_volume /var/gfs_cli_vol
-```
